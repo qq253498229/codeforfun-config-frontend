35182d678e4c78b6e2926633a547ed83666b6d42
大胃王 <253498229@qq.com>
2020-06-06 18:05:41 +0800

    添加模块

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo: '/home'},
-    {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)}
+    {path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+    {path: 'project', loadChildren: () => import('./pages/project/project.module').then(m => m.ProjectModule)},
+    {path: 'env', loadChildren: () => import('./pages/env/env.module').then(m => m.EnvModule)},
+    {path: 'conf', loadChildren: () => import('./pages/conf/conf.module').then(m => m.ConfModule)},
 ];
 
 @NgModule({
