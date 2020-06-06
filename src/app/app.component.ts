901ae96b016e9ae607d6fce53587e08cd64cb650
大胃王 <253498229@qq.com>
2020-06-06 17:34:06 +0800

    添加lodash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as _ from 'lodash';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  isCollapsed = false;
+export class AppComponent implements OnInit {
+    isCollapsed = false;
+
+    ngOnInit(): void {
+        let list = _.drop([1, 2, 3]);
+        console.log(list)
+    }
+
 }
