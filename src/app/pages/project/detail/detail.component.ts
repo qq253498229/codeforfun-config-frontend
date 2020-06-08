52257d56158ec33c60e957088debd0fd752506b4
大胃王 <253498229@qq.com>
2020-06-08 10:38:36 +0800

    update

--- src/app/pages/project/detail/detail.component.ts
+++ src/app/pages/project/detail/detail.component.ts
@@ -9,6 +9,7 @@ import {HttpClient} from "@angular/common/http";
 import {ProjectService} from "../project.service";
 import {Observable, Observer, timer} from "rxjs";
 import * as _ from 'lodash'
+import {map} from "rxjs/operators";
 
 @Component({
     selector: 'app-detail',
@@ -21,75 +22,18 @@ export class DetailComponent implements OnInit {
 
 
     userNameAsyncValidator(control: FormControl) {
-        //todo
-        return new Observable((observer: Observer<ValidationErrors | null>) => {
-            const func = _.debounce(() => {
-                console.log(control.value)
-                if (control.value === '123') {
-                    observer.next({error: true, duplicated: true})
+        // fixme 重复发送http请求
+        return this.service.checkName(control.value).pipe(
+            map(res => {
+                if (res) {
+                    return {}
                 } else {
-                    observer.next(null)
+                    return {error: true, duplicated: true}
                 }
-                observer.complete()
-            }, 2000, {maxWait: 5000})
-            func()
-        })
-        // const numbers = timer(1000);
-        // numbers.subscribe(x => {
-        //     console.log(x)
-        //     console.log(control.value)
-        // });
-        // return numbers
-
-        // return timer(500).switchMap(()=>{
-        //     return Observable.of({availability: true})
-        // });
-
-        // return this.http.get(`/api/config/project/checkName/${control.value}`).pipe(
-        //     debounceTime(1000),
-        //     distinctUntilChanged()
-        // )
-
-        // return new Observable((observer: Observer<ValidationErrors | null>) => {
-        //     const func = _.debounce(() => {
-        //         console.log(control.value)
-        //         if (control.value === '123') {
-        //             observer.next({error: true, duplicated: true})
-        //         } else {
-        //             observer.next(null)
-        //         }
-        //         observer.complete()
-        //     }, 300)
-        //
-        //     func()
-        // })
+            })
+        )
     }
 
-    // return new Observable((observer: Observer<ValidationErrors | null>) => {
-    //     console.log(control.value)
-    //     console.log(that.http)
-    //     if (control.value === '123') {
-    //         observer.next({error: true, duplicated: true})
-    //     } else {
-    //         observer.next(null)
-    //     }
-    //     observer.complete()
-    // })
-
-    // userNameAsyncValidator = (control: FormControl) =>
-    //     new Observable((observer: Observer<ValidationErrors | null>) => {
-    //         setTimeout(() => {
-    //             this.service.checkName(control.value).subscribe(res => {
-    //                 console.log('res', res)
-    //                 if (res) {
-    //                     observer.next(null);
-    //                 } else {
-    //                     observer.next({error: true, duplicated: true});
-    //                 }
-    //                 observer.complete();
-    //             })
-    //         }, 1000)
-    //     });
 
     submitForm(): void {
         for (const i in this.form.controls) {
