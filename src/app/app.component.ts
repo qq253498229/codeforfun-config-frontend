fde2b9c35e468920149724dbe3d49aed207fe5a0
大胃王 <253498229@qq.com>
2020-06-11 19:51:49 +0800

    添加快捷键

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ProjectService} from "./pages/project/project.service";
+import {Hotkey, HotkeysService} from "angular2-hotkeys";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-root',
@@ -13,7 +15,25 @@ export class AppComponent implements OnInit {
 
     constructor(
         private projectService: ProjectService,
+        private hotkey: HotkeysService,
+        private router: Router,
     ) {
+        this.hotkey.add(new Hotkey(['alt+0', 'option+0'], (): boolean => {
+            this.router.navigate(['/project'])
+            return false; // Prevent bubbling
+        }, undefined, '跳转到项目列表'));
+        this.hotkey.add(new Hotkey(['alt+1', 'option+1'], (): boolean => {
+            this.router.navigate(['/app'])
+            return false; // Prevent bubbling
+        }, undefined, '跳转到主页'));
+        this.hotkey.add(new Hotkey(['alt+2', 'option+2'], (): boolean => {
+            this.router.navigate(['/env'])
+            return false; // Prevent bubbling
+        }, undefined, '跳转到环境列表'));
+        this.hotkey.add(new Hotkey(['alt+3', 'option+3'], (): boolean => {
+            this.router.navigate(['/conf'])
+            return false; // Prevent bubbling
+        }, undefined, '跳转到配置列表'));
     }
 
     ngOnInit(): void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
 import {SharedModule} from "./pages/shared/shared.module";
 import {NzMenuModule} from "ng-zorro-antd";
+import {HotkeyModule} from "angular2-hotkeys";
 
 registerLocaleData(zh);
 
@@ -23,6 +24,16 @@ registerLocaleData(zh);
         BrowserAnimationsModule,
         SharedModule,
         NzMenuModule,
+        HotkeyModule.forRoot({
+            //禁止热键提示，默认false
+            disableCheatSheet: false,
+            //快捷键，默认?
+            cheatSheetHotkey: '?',
+            //是否允许使用ESC快捷键关闭，默认false
+            cheatSheetCloseEsc: false,
+            cheatSheetCloseEscDescription: '隐藏帮助页面',
+            cheatSheetDescription: '显示 / 隐藏帮助页面'
+        })
     ],
     providers: [{provide: NZ_I18N, useValue: zh_CN}],
     bootstrap: [AppComponent]
